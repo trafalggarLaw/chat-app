83e64d6f1db3cbbcffe71461f3dcef8bbf38a331
Harsh Singh <harshsingh@Harshs-MacBook-Air.local>
2025-03-17 03:10:10 +0530

    temp

---
+++ auth-backend/routes/users.route.js
@@ -0,0 +1,11 @@
+import express from "express"
+import verifyToken from "../middleware/verifyToken.js";
+import getUsers from "../controllers/users.controller.js";
+
+
+const router = express.Router();
+
+
+router.get('/', verifyToken, getUsers);
+
+export default router;
